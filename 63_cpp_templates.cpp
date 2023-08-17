e38a31a347eccfd9c9768094fe9530963f3322b2
himalraj07 <himalrajbhusal@gmail.com>
2023-08-17 09:22:01 +0545

    C++ Templates : Must for Competitive Programming

--- 63_cpp_templates.cpp
+++ 63_cpp_templates.cpp
@@ -2,3 +2,28 @@
     C++ Templates : Must for Competitive Programming
 */
 
+/*
+    why use templates ?
+    --> Follows DRY Principle
+    --> Generic Programming
+*/
+
+/*
+    Syntax for Templates
+    template < class T > // T can be int,float,char etc.
+    class vector
+    {
+        T *arr
+        public : 
+        vector (T *arr)
+        {
+            // code
+            // & many other methods
+        }
+    }
+    int main ()
+    {
+        vector <int> myvec (ptr);
+        vector <float> myfvec (ptr);
+    }
+*/
