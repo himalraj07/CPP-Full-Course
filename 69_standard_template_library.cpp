2582d88ec5bfd30ede6acf6a1b6c1331d30d17a6
himalraj07 <himalrajbhusal@gmail.com>
2023-08-19 17:43:16 +0545

    The C++ Standard Template Library (STL)

---
+++ 69_standard_template_library.cpp
@@ -0,0 +1,14 @@
+/*
+    69. The C++ Standard Template Library (STL)
+*/
+
+/*
+    Components of STL
+    1. Containers --> storts data, use template classes
+    2. Algorithms --> Sorting, Searching, Use template Functions
+    3. Iterations --> It is a object which points to an element in a conttainer. Handled just like poiners. Connects algorithm with containers.
+*/
+
+/*
+    STL is used because its a good idea not to reinvent the wheel.
+*/
