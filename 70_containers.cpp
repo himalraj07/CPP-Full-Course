341df3f32a093ed6bc938075e08e56c1efc68fe7
himalraj07 <himalrajbhusal@gmail.com>
2023-08-19 18:09:00 +0545

    Containers in C++ STL

---
+++ 70_containers.cpp
@@ -0,0 +1,29 @@
+/*
+    70. Containers in C++ STL
+*/
+
+/*
+    There are 3 types of containers
+    1. Sequence Containers      --> Linear Pattern (Vector, List, Dequeue)
+    2. Associative Containers   --> Direct Accessible, Stores data in tree like structure (Set, Multiset, Map, Multimap)
+    3. Derived Containers       --> Real world modeling (Stack, Queue, Priority-Queue)
+*/
+
+/*
+    When to use ?
+
+    # Sequence Containers
+    1. Vector --> Random Access = Fast (Compared to array)
+    Insertion/Deletion at the Medium = slow
+    Insertion/Deletion at the end = Fast
+
+    2. List --> Random Access = Slow
+    Middle Insertion = Fast
+    Deletion/Insertion at the end = fast
+
+    # Associative Containers
+    All operations Fast except Random Access
+
+    # Derived Containers
+    Depends --> Data Structure
+*/
